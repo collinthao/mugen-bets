864fb21e4bd6a22de87d652c58de714d9f6a32bf
collin <collin@example.com>
2026-08-23 21:24:32 -0500

    instrument: set exe cwd and add startup/error logs to capture silent crashes on other machines

--- Mugen-Bets/main.cpp
+++ Mugen-Bets/main.cpp
@@ -7,20 +7,54 @@
 #include <sstream>
 #include <thread>
 #include <chrono>
+#include <filesystem>
+#include <ctime>
 #include "mugenBattleManager.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main()
 {
-	while (true)
-	{
-		MugenBattleManager::StartBattle();
-		MugenBattleManager::WaitForBattleEnd();
-		MugenBattleManager::SetMatchResult();
-		MugenBattleManager::SetCharactersStatsJSON();
+	try {
+#ifdef _WIN32
+		// Ensure working directory is the executable folder so relative assets are found
+		char exePath[MAX_PATH] = {0};
+		if (GetModuleFileNameA(NULL, exePath, MAX_PATH)) {
+			std::string p(exePath);
+			auto pos = p.find_last_of("\\/");
+			if (pos != std::string::npos) {
+				std::string dir = p.substr(0, pos);
+				SetCurrentDirectoryA(dir.c_str());
+			}
+		}
+#endif
 
-		std::chrono::duration<double, std::milli> time{ 10000 };
-		std::this_thread::sleep_for(time);
+		std::ofstream log("mugen-start.log", std::ios::app);
+		log << "Started at: " << std::time(nullptr) << " cwd=" << std::filesystem::current_path().string() << std::endl;
 
+		while (true)
+		{
+			MugenBattleManager::StartBattle();
+			MugenBattleManager::WaitForBattleEnd();
+			MugenBattleManager::SetMatchResult();
+			MugenBattleManager::SetCharactersStatsJSON();
+
+			std::chrono::duration<double, std::milli> time{ 10000 };
+			std::this_thread::sleep_for(time);
+
+		}
 	}
+	catch (const std::exception &e) {
+		std::ofstream log("mugen-error.log", std::ios::app);
+		log << "Unhandled exception: " << e.what() << std::endl;
+		return 1;
+	}
+	catch (...) {
+		std::ofstream log("mugen-error.log", std::ios::app);
+		log << "Unhandled unknown exception" << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
